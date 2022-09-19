313449ff834535b44bd9a3f2b0e06843bbaace1f
Th1nhNg0 <ngophuthinh2001@gmail.com>
2022-09-19 18:31:48 +0700

    update

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,8 +3,17 @@ import { AiOutlineArrowDown } from "react-icons/ai";
 export default function Banner() {
   return (
     <Center pos="relative" w="full" minH="100vh">
-      <Box pos="absolute">
-        <video autoPlay muted loop>
+      <Box pos="absolute" w="full" h="full">
+        <video
+          autoPlay
+          muted
+          loop
+          style={{
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+          }}
+        >
           <source
             src="/mixkit-hands-of-a-man-playing-on-a-computer-43527.mp4"
             type="video/mp4"
@@ -12,7 +21,13 @@ export default function Banner() {
         </video>
       </Box>
       <Box pos="absolute" w="full" h="full" bg="rgba(0,0,0,0.3)"></Box>
-      <Image pos="relative" src="/EUTLabs_logo.png" maxW="2xl" />
+      <Image
+        pos="relative"
+        src="/EUTLabs_logo.png"
+        maxW="2xl"
+        w="full"
+        px="5"
+      />
     </Center>
   );
 }
